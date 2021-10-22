739357a2170f703e4aa634fe5eee3d002f5a214c
pizzi9523 <pizzitolagiuseppe@live.it>
2021-10-22 15:01:09 +0200

    BONUS creo tre schede con i miei dati

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,19 +3,19 @@ const members_team = [
     {
         name: "Giuseppe",
         role: "Student",
-        image: "https://picsum.photos/id/1/200/300"
+        image: "https://picsum.photos/id/1025/200/300"
     },
 
     {
-        name: "Ben Green",
+        name: "Elizabeth Green",
         role: "CEO",
-        image: "https://picsum.photos/id/2/200/300"
+        image: "https://picsum.photos/id/1011/200/300"
     },
 
     {
         name: "Fabio",
         role: "Teacher",
-        image: "https://picsum.photos/id/3/200/300"
+        image: "https://picsum.photos/id/1005/200/300"
     }
 
 ]
@@ -27,4 +27,16 @@ for (let i = 0; i < members_team.length; i++) {
     }
 }
 
+let card = "";
+for (let i = 0; i < members_team.length; i++) {
+    card +=
+        `<div class="card" style="width: 18rem;">
+        <img src=${members_team[i]["image"]} class="card-img-top my-3" alt="...">
+        <div class="card-body">
+        <h5 class="card-title">${members_team[i]["name"]}</h5>
+        <p class="card-text">${members_team[i]["role"]}</p>
+        </div>
+    </div>`
+}
 
+document.querySelector(".row").innerHTML = card;
